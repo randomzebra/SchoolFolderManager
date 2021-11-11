7bd9e38caeaa1288f898daad7e89de1ec12fad8a
Vincent Fang <vincentfang0@gmail.com>
2021-11-10 21:44:38 -0500

    Documentation and Error Handeling
    Documentation and error handeling before moving on to commandline control and Google drive archiving

--- Backend/Main.py
+++ Backend/Main.py
@@ -17,8 +17,9 @@ class Main:
         """Constructor Method: Creates config and Data jsons if not present in the PWD, else loads them into
         dictionaries
         Attempts to create an Archive Folder in the path specified in config
+        Args:
         :param None:
-        :return None:
+        
         """
 
         # Tags: 0 = Desktop     1 = Archive     2 = liveData    3 = archiveData     4 = Path    
@@ -65,8 +66,9 @@ class Main:
     # TODO handle Archive dumping before dumping data.json
     def dump(self):
         """Loads any data and config changes into the json
+        
+        Args
         :param None:
-        :return None:
         """
         with open(os.path.join(self.backend,'data.json'),'w') as f:
             json.dump(self.data,f)
@@ -76,13 +78,14 @@ class Main:
     
     def addFolder(self, Name):
         """Adds a folder, adds it to the Desktop directory
+        Args:
         :param Name: Name of folder to add
         :type Name: Mandetory str 
 
+        Raises:
         :raise FileExisistsError: File already exists in Desktop
         :raise NotADirectoryError: User name is a windows reserved folder name
-        :raise OSError: User name has illegal characteres
-        :return None: 
+        :raise OSError: User name has illegal characterers
         """
         if (type(Name) != str):
             raise ValueError("Folder Name can only be a String")
@@ -113,83 +116,151 @@ class Main:
 
     # TODO Impliment error messages into the GUI 
     def addFolders(self, List):
-            for name in List:
-                try:
-                    self.addFolder(name)
-                except FileExistsError:
-                    print(name + " already exists in " + self.config[self.T0])
-                except NotADirectoryError:
-                    print(name + " is a reserved folder name!")
-                except FileNotFoundError:
-                    print("Desktop directory lost! Reselect desktop directory!")
-                except OSError:
-                    print(name + " has invalid character!")
+        """ Impliments addFolder functionality onto a list of strings, and catches exceptions
+        Args:
+           :param List: List of directory names to add
+           :type List: String List
+        """
+        for name in List:
+            try:
+                self.addFolder(name)
+            except FileExistsError:
+                print(name + " already exists in " + self.config[self.T0])
+            except NotADirectoryError:
+                print(name + " is a reserved folder name!")
+            except FileNotFoundError:
+                print("Desktop directory lost! Reselect desktop directory!")
+            except OSError:
+                print(name + " has invalid character!")
 
-    # TODO add argument type check
+    
     def changeDesktop(self, Path):
         """Changes the desktop folder in config to a new location. DOES NOT move folders
+        
+        Args: 
         :param Path: Path of new desktop directory
         :type Path: Mandetory str
-
-        :return None:
+        
+        Raises:
+        :raise ValueError: One of two cases: path is not a string, or path is an invalid path, likely assuming higher
+        level directories that do not exist
         """
+        if (type(Path) != str):
+            raise ValueError("Path can only be a String")
+        elif os.path.exists(os.path.dirname(Path)):
+            raise ValueError("Path is invalid!")
+        
         self.config[self.T0] = Path
         liveList = self.data[self.T2].keys()
         for dir in liveList:
             self.data[self.T2][dir][self.T5] = False
 
-    # TODO add argument type check
+    
     def changeArchive(self,Path):
         """Changes the archive folder in config to a new location. DOES NOT move folders
+        Args:
         :param Path: Path of new desktop directory
         :type Path: Mandetory str 
-
-        :return None:
+        
+        Raises:
+        :raise ValueError: One of two cases: path is not a string, or path is an invalid path, likely assuming higher
+        level directories that do not exist
         """
+        if (type(Path) != str):
+            raise ValueError("Path can only be a String")
+        elif os.path.exists(os.path.dirname(Path)):
+            raise ValueError("Path is invalid!")
         self.config[self.T1] = Path
         archList = self.data[self.T3].keys()
         for dir in archList:
             self.data[self.T3][dir][self.T6] = False
 
-    # TODO add argument type check
+    
     def archive(self, Name):
         """ Moves a paticular file to archive. Moves folder and all contents. 
-            Assumes Name is in liveData
+            Args:
             :param Name: Name of file to move
             :type Name: Mandetory str
+            
+            
+            Raises:
+            :raise KeyError: Name of file to move is not in Archive directory
+            :raise shutil.Error: shutil has encountered an error, either the path or the target is invalid, or 
+            the directory to move already exists in the target directory
         """
         try:
             path = self.data[self.T2][Name][self.T4]
         except KeyError:
             raise
-        self.data[self.T2][Name][self.T4] = shutil.move(path, self.config[self.T1])  
-        self.data[self.T2][Name][self.T5] = False
-        self.data[self.T2][Name][self.T6] = True
-        self.data[self.T3][Name] = self.data[self.T2].pop(Name)
+        try:
+            self.data[self.T2][Name][self.T4] = shutil.move(path, self.config[self.T1])  
+            self.data[self.T2][Name][self.T5] = False
+            self.data[self.T2][Name][self.T6] = True
+            self.data[self.T3][Name] = self.data[self.T2].pop(Name)
+        except shutil.Error:
+            raise
         
-    # TODO impliment dearchive, which should provide backwards functionality from archive
     def deArchive(self, Name):
+        """Move from archive to the desktop 
+
+        Args:
+            :param Name: Name of the file to dearchive
+            :type Name: String
+            
+        Raises:
+            :raise KeyError: Name is not in archive
+            :raise shutil.Error: shutil custom error, indicating invalid paths or prexisitng directory in target. 
+            Specifics in the actual Error object, to be printed
+        """
         try:
             path = self.data[self.T3][Name][self.T4]
         except KeyError:
             raise
-        self.data[self.T3][Name][self.T4] = shutil.move(path, self.config[self.T0])
-        path = self.data[self.T3][Name][self.T5] = True
-        path = self.data[self.T3][Name][self.T6] = False
-        self.data[self.T2][Name] = self.data[self.T3].pop(Name)
+        try:
+            self.data[self.T3][Name][self.T4] = shutil.move(path, self.config[self.T0])
+            path = self.data[self.T3][Name][self.T5] = True
+            path = self.data[self.T3][Name][self.T6] = False
+            self.data[self.T2][Name] = self.data[self.T3].pop(Name)
+        except shutil.Error:
+            raise
 
     
     def archives(self, List):
+        """Impliments archive functinality on a List of strings. Catches Errors raised by archive per name
+
+        Args:
+            :param Name: Name of the file to archive
+            :type Name: String List
+        """
         for name in List:
             try:
                 self.archive(name)
             except KeyError:
                 print(name + " is not in Desktop!")
+            except shutil.Error as e:
+                print(e)
         
-        
+    def deArchives(self, List):
+        """Impliments dearchive functinality on a List of strings. Catches Errors raised by archive per name
 
-    # TODO impliment refresh, should dump any changes to jsons, or check if folders are present
+        Args:
+            :param Name: Name of the file to dearchive
+            :type Name: String List
+        """
+        for name in List:
+            try:
+                self.deArchive(name)
+            except KeyError:
+                print(name + " is not in Desktop!")
+            except shutil.Error as e:
+                print(e)
+
+    
     def refresh(self):
+        """check live and archiveData for tracked folder presense. Updates appropriate boolean fields to reflect status
+        Dumps jsons on completion. This function does not handle cases where the jsons loose track of the file locations
+        after archiving or dearchiving: that is handled in the repopDesktop or repopArchive functions
+        """
         keysdata = self.data[self.T2].keys()
         keysarc = self.data[self.T3].keys()
         for name in keysdata:
@@ -205,11 +276,20 @@ class Main:
         self.dump()
         
 
-
-    # impliment repop, which should repopulate a paticular folder into either the Archive or Desktop
-    # dir should be a boolean 0 for desktop, 1 for archive. This should not delete any folders, but simply
-    # create another. This should also update the entries path and inArch/Desk fields
+    #TODO check the appropriate directory to see if the the directory exists but reads as untracked. Probably a variation of refresh, if not refresh
     def repop(self, Name, dir):
+        """Recreate a paticular directory in data but not present actually. Dir is boolean which refers to which 
+        dictionary it belongs to: data or archive. 0 for data, 1 for archive
+
+        Args:
+            :param Name: Name of file
+            :type Name: String
+            :param dir: Boolean representing master directory
+            :type dir: Boolean
+        
+        Raise:
+            :raise KeyError: Name is not present in specified dictionary
+        """
         if dir:
             try:
                 os.mkdir(os.path.join(self.config[self.T1], Name))
@@ -223,8 +303,12 @@ class Main:
             except KeyError:
                 raise
     
-    # TODO consider edgecase where os.path exists but is not in data
+    # TODO catch shutil.Error. Check desktop directory for the 
     def repopDesktop(self):
+        """Impliments repop functionality for all files in liveData. Checks the tracked path of the file,
+        and if it exists, move it. Otherwise, repop. 
+        
+        """
         keys = self.data[self.T2].keys()
         for Name in keys:
             if not self.data[self.T2][Name][self.T5]:
@@ -234,14 +318,12 @@ class Main:
                     self.data[self.T2][Name][self.T5] = True
                     self.data[self.T2][Name][self.T4] = os.path.join(self.config[self.T0], Name)
                 else:
-                    try:
-                        self.repop(Name,0)
-                    except KeyError:
-                        print(Name + " not in data originally!")
+                    self.repop(Name,0)
+                    
         
 
 
-    # TODO consider edgecase where os.path exists but is not in data
+    # TODO catch shutil.error
     def repopArchive(self):
         keys = self.data[self.T3].keys()
         for Name in keys:
@@ -260,9 +342,11 @@ class Main:
                     except KeyError:
                         print(Name + " not in data originally!")
 
-    # TODO Move all folders tracked in desktop to archive
     def rollover(self):
-        return
+        """[summary]
+        """
+        keys = self.data[self.T2].keys()
+        self.archives(keys)
 
 
 
